e47785c22edd406128b4b0ada9ab0e0f7deeb01d
nayaksanjeet <40313847+nayaksanjeet@users.noreply.github.com>
2018-06-16 16:35:48 +0530

    Add files via upload
    join and yield

---
+++ src/main/java/com/sanjeet/corejava/corejava/multithreading/YieldDemo.java
@@ -0,0 +1,26 @@
+package com.sanjeet.corejava.corejava.multithreading;
+
+public class YieldDemo {
+public static void main(String[] args) {
+	Runnable task1=()->{
+		//System.out.println("runnable run method");
+		for(int i=0;i<5;i++){
+			System.out.println("Producer "+i);
+			Thread.yield();
+		}
+	};
+	Thread t1=new Thread(task1);
+	Runnable task2=()->{
+		for(int i=0;i<5;i++){
+			System.out.println("Consumer "+i);
+			Thread.yield();
+		}
+	};
+	Thread t2=new Thread(task2);
+	t1.setPriority(Thread.MAX_PRIORITY);
+	t2.setPriority(Thread.MIN_PRIORITY);
+	t1.start();
+	t2.start();
+	System.out.println("Completed");
+}
+}
